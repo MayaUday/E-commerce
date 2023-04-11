0e7d4f31ac0f5689464f11bc0de443b5386fb8f8
MayaUday <nidhimehendale99@gmail.com>
2023-04-11 15:20:29 +0530

    added Addion/substraction of price

--- Ecommerce.js
+++ Ecommerce.js
@@ -1,3 +1,5 @@
+const total = document.getElementById('total');
+
 function saveOnDatabase(event){
     event.preventDefault();
     const sPrice = parseFloat(event.target.price.value);
@@ -8,26 +10,32 @@ function saveOnDatabase(event){
       pName : pName
     };
     
-    axios.post("https://crudcrud.com/api/3a744bb0cd754d77bf439f7f7d4b3e2d/productsData" ,Productobj)
+    axios.post("https://crudcrud.com/api/18f97a9873c54c90a59733bc93191bbc/productsData" ,Productobj)
      .then( (response) => {
         saveProductOnScreen(response.data);
-    
      })
      .catch((err) => {
         document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong1<h4>";
         //console.log(obj);
      })
-    updateTotal(Productobj.sPrice);
+     addTotal(Productobj.sPrice);
      
 }
 
-function updateTotal(price) {
-    const total = document.querySelector('.total');
+function addTotal(price) {
     let currentTotal = parseFloat(total.innerHTML);
     if(isNaN(currentTotal)) {
       currentTotal = 0;
     }
     total.innerHTML = currentTotal + price;
+  }
+
+  function deleteTotal(price) {
+    let currentTotal = parseFloat(total.innerHTML);
+    if(isNaN(currentTotal)) {
+      currentTotal = 0;
+    }
+    total.innerHTML = currentTotal - price;
   }
 
 
@@ -39,35 +47,35 @@ function saveProductOnScreen(Productobj){
     const deleteButton = document.createElement('input');
     deleteButton.type = "button";
     deleteButton.value = "Delete Product";
+     let sum=0;
     deleteButton.onclick = () => {
-     axios.delete("https://crudcrud.com/api/3a744bb0cd754d77bf439f7f7d4b3e2d/productsData/" + Productobj._id)
+     axios.delete("https://crudcrud.com/api/18f97a9873c54c90a59733bc93191bbc/productsData/" + Productobj._id)
     .then((response) => {
-        parentEle.removeChild(childEle);
+      parentEle.removeChild(childEle);   
+      deleteTotal(Productobj.sPrice);
     })
     .catch((err) => {
         console.log(err);
     });
      //  localStorage.removeItem(Productobj.pName);
-       parentEle.removeChild(childEle);
+       //parentEle.removeChild(childEle);
     }
-
     childEle.appendChild(deleteButton);
     parentEle.appendChild(childEle);
 
 }
 
 
-window.addEventListener("DOMContentLoaded" , () => {
-    axios.get("https://crudcrud.com/api/3a744bb0cd754d77bf439f7f7d4b3e2d/productsData")
-    .then( (response) => {
-
-     for(var i=0;i<response.data.length ;i++){
-       saveProductOnScreen(response.data[i]);
-     }
-    
-    })
-    .catch( (err) => {
-     document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong2<h4>";
-    })
-})
+  window.addEventListener("DOMContentLoaded" , () => {
+      axios.get("https://crudcrud.com/api/18f97a9873c54c90a59733bc93191bbc/productsData")
+      .then( (response) => {
+       for(let i=0;i<response.data.length ;i++){
+          saveProductOnScreen(response.data[i]);  
+          addTotal(response.data[i].sPrice);           
+       }    
+      })
+      .catch( (err) => {
+       document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong2<h4>";
+      })
+  })
 
